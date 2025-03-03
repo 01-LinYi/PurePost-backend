959de80f90abf60075d77e17b5da84280f35bca5
Yangliuqing09 <willow33@connect.hku.hk>
2025-03-02 22:49:17 -0500

    Update urls.py

--- purepost/urls.py
+++ purepost/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include("purepost.auth_service.urls")),
     path('users/', include('purepost.user_service.urls')),
+    path('content/', include('purepost.content_moderation.urls')),
 ]
