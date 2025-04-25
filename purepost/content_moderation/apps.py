c9093e0bcf29a5ffe502027d68bf46e046436a33
01-LinYi <yliner@connect.ust.hk>
2025-04-24 23:03:15 -0400

    Revert "Merge scheduling BE to main"
    This reverts commit 850371a7286ab14d0ffe789920d46a9ed9293371, reversing
    changes made to 66b6f9bf0e7cebfc5b11f8790c36b3eb4df6e67f.

--- purepost/content_moderation/apps.py
+++ purepost/content_moderation/apps.py
@@ -1,6 +1,4 @@
-# content_moderation/apps.py
 from django.apps import AppConfig
-import sys
 
 class ContentModerationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -8,8 +6,3 @@ class ContentModerationConfig(AppConfig):
 
     def ready(self):
         import purepost.content_moderation.signals  # noqa Import signals
-        # Prevent scheduler from running twice or during migrations
-        if 'runserver' in sys.argv:
-            # Only import and start scheduler when the runserver command is used
-            from . import scheduler
-            scheduler.start()
